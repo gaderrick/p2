eeda6c35557c1d716e632c4b07fd93a6cc23a3c0
Gerald Derrick <jerry.derrick@gmail.com>
2018-02-11 07:24:24 -0600

    Added scrabble images to the project

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 + Production URL: <http://p2.gaderrick.me>
 
 ## Outside resources
-*your list of outside resources go here*
+[Scrabble Images](http://www.echopulse.net/lj/images/scrabble-icons-pack-crystalxp.net-en-1041%20Folder/png/scrabble-uk/)
 
 ## Code style divergences
 *list any divergences from PSR-1/PSR-2 and course guidelines on code style*
--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CSCI E-15 | Project 2 </title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          rel="stylesheet"
+          crossorigin="anonymous">
+    <link href="styles.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 
