c73b644520306722c4ead1d06f5b68fdcae98d5b
Gerald Derrick <jerry.derrick@gmail.com>
2018-02-17 11:18:33 -0600

    Added Form class to the project and fixed quotes (turned doubles to singles)

--- index.php
+++ index.php
@@ -1,98 +1,97 @@
 <?php
-require("myhelpers.php");
-require("helpers.php");
-require("scoring.php");
+require('myhelpers.php');
+require('helpers.php');
+require('scoring.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
     <title>CSCI E-15 | Project 2 - Scrabble Scoring App</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          rel="stylesheet"
-          crossorigin="anonymous">
-    <link href="styles.css" rel="stylesheet">
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+          rel='stylesheet'
+          crossorigin='anonymous'>
+    <link href='styles.css' rel='stylesheet'>
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row" style="padding-bottom: 5px">
-        <div class="col-sm-12">
-            <img class="img-responsive title" src="images/scrabble_score.png" alt="Scrabble Score Logo" id="logo">
+<div class='container-fluid'>
+    <div class='row' style='padding-bottom: 5px'>
+        <div class='col-sm-12'>
+            <img class='img-responsive title' src='images/scrabble_score.png' alt='Scrabble Score Logo' id='logo'>
         </div>
     </div>
-    <div class="row">
-        <?php if (isset($_GET["userWord"]) && ($displaySuccess)): ?>
-            <div class="col-sm-12 formText messageArea" style="background-color: mediumseagreen">
+    <div class='row'>
+        <?php if (isset($_GET['userWord']) && ($displaySuccess)): ?>
+            <div class='col-sm-12 formText messageArea alert alert-success'>
                 Your Word:<br>
                 <?php for ($i = 0; $i < strlen($wordToCheck); $i++): ?>
-                    <img src="images/<?= $wordToCheck[$i] ?>.png"
-                         alt="<?= $wordToCheck[$i] ?> | <?= $letters[$wordToCheck[$i]] ?>"
-                         class="responsive-image-small">
+                    <img src='images/<?= $wordToCheck[$i] ?>.png'
+                         alt='<?= $wordToCheck[$i] ?> | <?= $letters[$wordToCheck[$i]] ?>'
+                         class='responsive-image-small'>
                 <?php endfor ?>
                 <br>
                 Scored <?= $wordScore ?> points with the following options:<br><br>
-                <?php if ($multiplier == "double"): ?>
-                    <img class="img-responsive" src="images/double.png" alt="2x Word Score" id="double">
-                <?php elseif ($multiplier == "triple"): ?>
-                    <img class="img-responsive" src="images/triple.png" alt="3x Word Score" id="triple">
+                <?php if ($multiplier == 'double'): ?>
+                    <img class='img-responsive' src='images/double.png' alt='2x Word Score' id='double'>
+                <?php elseif ($multiplier == 'triple'): ?>
+                    <img class='img-responsive' src='images/triple.png' alt='3x Word Score' id='triple'>
                 <?php endif ?>
             </div>
-        <?php elseif (isset($_GET["userWord"]) && (!$displaySuccess)): ?>
-            <div class="col-sm-12 formText messageArea"
-                 style="background-color: crimson;line-height: 75px">
-                Sorry, <?= ($wordToCheck <> "" ? $wordToCheck : "a blank") ?> is not a valid word
+        <?php elseif (isset($_GET['userWord']) && (!$displaySuccess)): ?>
+            <div class='col-sm-12 formText messageArea alert alert-danger'
+                 style='line-height: 75px'>
+                Sorry, <?= ($wordToCheck <> '' ? $wordToCheck : 'a blank') ?> is not a valid word
             </div>
         <?php else: ?>
-            <div class="col-sm-12 formText messageArea"
-                 style="background-color:lightgoldenrodyellow; line-height: 75px">Enter a word below to
-                score
+            <div class='col-sm-12 formText messageArea alert alert-info'
+                 style='line-height: 75px'>Enter a word below to score
             </div>
         <?php endif ?>
     </div>
-    <div class="row">
-        <div class="col-sm-12 mainArea">
-            <form method="GET" action="index.php" name="main">
-                <div class="row" style="padding-top: 5px; padding-bottom: 15px">
-                    <div class="col-sm-6 formText" style="text-align: right">
-                        <label for="userWord">Word to score:</label>
+    <div class='row'>
+        <div class='col-sm-12 mainArea'>
+            <form method='GET' action='index.php' name='main'>
+                <div class='row' style='padding-top: 5px; padding-bottom: 15px'>
+                    <div class='col-sm-6 formText' style='text-align: right'>
+                        <label for='userWord'>Word to score:</label>
                     </div>
-                    <div class="col-sm-6">
-                        <input type="text" name="userWord" style="width: 100%" id="userWord">
+                    <div class='col-sm-6'>
+                        <input type='text' name='userWord' style='width: 100%' id='userWord'>
                     </div>
                 </div>
-                <div class="row" style="padding-bottom: 15px">
-                    <div class="col-sm-6 formText" style="text-align: right">
-                        <label for="multiplier">Score Multiplier:</label>
+                <div class='row' style='padding-bottom: 15px'>
+                    <div class='col-sm-6 formText' style='text-align: right'>
+                        <label for='multiplier'>Score Multiplier:</label>
                     </div>
-                    <div class="col-sm-6">
-                        <select name="multiplier" id="multiplier">
-                            <option value="none">None</option>
-                            <option value="double">Double Word</option>
-                            <option value="triple">Triple Word</option>
+                    <div class='col-sm-6'>
+                        <select name='multiplier' id='multiplier'>
+                            <option value='none'>None</option>
+                            <option value='double'>Double Word</option>
+                            <option value='triple'>Triple Word</option>
                         </select>
                     </div>
                 </div>
-                <div class="row" style="padding-bottom: 15px">
-                    <div class="col-sm-6 formText" style="text-align: right">
-                        <label for="bingo">Bingo! (+50 pts)?</label>
+                <div class='row' style='padding-bottom: 15px'>
+                    <div class='col-sm-6 formText' style='text-align: right'>
+                        <label for='bingo'>Bingo! (+50 pts)?</label>
                     </div>
-                    <div class="col-sm-6">
-                        <input type="checkbox" name="bingo" id="bingo">
-                    </div>
-                </div>
-                <div class="row" style="padding-bottom: 15px">
-                    <div class="col-sm-6 formText" style="text-align: right">
-                        <label for="spelling">Check word spelling?</label>
-                    </div>
-                    <div class="col-sm-6">
-                        <input type="checkbox" name="spelling" id="spelling">
+                    <div class='col-sm-6'>
+                        <input type='checkbox' name='bingo' id='bingo'>
                     </div>
                 </div>
-                <div class="row" style="padding-top:25px; padding-bottom: 15px">
-                    <div class="col-sm-12 formText centerContents">
-                        <input type="submit" value="Score the Word" class="btn">
+                <div class='row' style='padding-bottom: 15px'>
+                    <div class='col-sm-6 formText' style='text-align: right'>
+                        <label for='spelling'>Check word spelling?</label>
+                    </div>
+                    <div class='col-sm-6'>
+                        <input type='checkbox' name='spelling' id='spelling'>
+                    </div>
+                </div>
+                <div class='row' style='padding-top:25px; padding-bottom: 15px'>
+                    <div class='col-sm-12 formText centerContents'>
+                        <input type='submit' value='Score the Word' class='btn'>
                     </div>
                 </div>
             </form>
--- scoring.php
+++ scoring.php
@@ -1,18 +1,18 @@
 <?php
 // Set local variables for all the possible _GET variables
-$wordToCheck = strtolower((isset($_GET["userWord"]) ? $_GET["userWord"] : ""));
-$spelling = (isset($_GET["spelling"]) && $_GET["spelling"] == "on" ? true : false);
-$bingo = (isset($_GET["bingo"]) && $_GET["bingo"] == "on" ? true : false);
-$multiplier = (isset($_GET["multiplier"]) ? $_GET["multiplier"] : "");
+$wordToCheck = strtolower((isset($_GET['userWord']) ? $_GET['userWord'] : ''));
+$spelling = (isset($_GET['spelling']) && $_GET['spelling'] == 'on' ? true : false);
+$bingo = (isset($_GET['bingo']) && $_GET['bingo'] == 'on' ? true : false);
+$multiplier = (isset($_GET['multiplier']) ? $_GET['multiplier'] : '');
 
-// Read the JSON file containing the default scores for letters into an array "$letters"
-$lettersJSON = file_get_contents("data/letters.json");
+// Read the JSON file containing the default scores for letters into an array '$letters'
+$lettersJSON = file_get_contents('data/letters.json');
 $letters = json_decode($lettersJSON, true);
 
 // If a spelling check was requested and the word to check is not blank look the word up
-if ($spelling && $wordToCheck <> "") {
+if ($spelling && $wordToCheck <> '') {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" . $wordToCheck);
+    curl_setopt($ch, CURLOPT_URL, 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/' . $wordToCheck);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'app_id: 1e45975c',
@@ -21,13 +21,13 @@ if ($spelling && $wordToCheck <> "") {
     $apiResult = trim(curl_exec($ch));
     curl_close($ch);
 } else {
-    $apiResult = "Not JSON";
+    $apiResult = 'Not JSON';
 }
 
 $isRealWord = isJson($apiResult);
 
-// Read the JSON file containing the default scores for letters into an array "$letters"
-$lettersJSON = file_get_contents("data/letters.json");
+// Read the JSON file containing the default scores for letters into an array '$letters'
+$lettersJSON = file_get_contents('data/letters.json');
 $letters = json_decode($lettersJSON, true);
 
 // Loop over each letter in $wordToCheck and add up its score based on the values in $letters
@@ -39,13 +39,13 @@ for ($i = 0; $i < strlen($wordToCheck); $i++) {
 }
 
 // Apply the multiplier and bingo score modification
-if ($wordToCheck <> "") {
-    if ($multiplier <> "") {
+if ($wordToCheck <> '') {
+    if ($multiplier <> '') {
         switch ($multiplier) {
-            case "double":
+            case 'double':
                 $wordScore = $wordScore * 2;
                 break;
-            case "triple":
+            case 'triple':
                 $wordScore = $wordScore * 3;
                 break;
         }
